52e1eabae735bb09ed6a1bbfd067e2bdc7707bd3
Vincent Noël <contact@vincent-noel.fr>
2020-08-26 10:25:45 +0200

    Added simple telomere maintenance example

---
+++ factory/examples/build_telomere_map.sh
@@ -0,0 +1,7 @@
+destdir=../../site/docroot/navicell/maps/telomere
+
+if [ -d $destdir ]; then echo "$0: TARGET_DIR $destdir already exists"; exit 1; fi
+
+mkdir $destdir
+
+sh ../scripts/run_factory.sh --config telomere_src/config --xrefs xrefs.txt --destination $destdir --verbose
