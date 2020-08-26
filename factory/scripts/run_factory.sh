6d839ef8a7f4c963712ae00e15789214802c333e
Vincent Noël <contact@vincent-noel.fr>
2020-08-26 10:09:25 +0200

    Cleanup classpath of useless libraries

--- factory/scripts/run_factory.sh
+++ factory/scripts/run_factory.sh
@@ -11,7 +11,11 @@ topdir=..
 
 libdir=$topdir/lib
 
-classpath=$libdir/navicell.jar:$libdir/binomlibext.jar:$libdir/cd4.jar:$libdir/celldesigner.jar:$libdir/CellDesigner401Jars.jar:$libdir/csml.jar:$libdir/cytoscape270.jar:$libdir/giny.jar:$libdir/imgscalr-lib-3.1-javadoc.jar:$libdir/imgscalr-lib-3.1-sources.jar:$libdir/imgscalr-lib-3.1.jar:$libdir/jwordpress/commons-collections-3.2.jar:$libdir/jwordpress/commons-configuration-1.5.jar:$libdir/jwordpress/commons-lang-2.3.jar:$libdir/jwordpress/jwordpress-0.5.1-cli.jar:$libdir/jwordpress/jwordpress-0.5.1.jar:$libdir/jwordpress/jwordpress-0.5.jar:$libdir/jwordpress/xmlrpc-client-1.1.jar:$libdir/MathMLIO.jar:$libdir/paxtools-4.2.1-no-jena.jar:$libdir/sbml.jar:$libdir/transpath.jar:$libdir/VDAOEngine.jar:$libdir/xgmml.jar:$topdir
+# Here I remove this from the classpath :
+# $libdir/CellDesigner401Jars.jar : Because of duplicates with cd4.jar
+# $libdir/celldesigner.jar : Because it is not used
+
+classpath=$libdir/navicell.jar:$libdir/binomlibext.jar:$libdir/cd4.jar:$libdir/csml.jar:$libdir/cytoscape270.jar:$libdir/giny.jar:$libdir/imgscalr-lib-3.1-javadoc.jar:$libdir/imgscalr-lib-3.1-sources.jar:$libdir/imgscalr-lib-3.1.jar:$libdir/jwordpress/commons-collections-3.2.jar:$libdir/jwordpress/commons-configuration-1.5.jar:$libdir/jwordpress/commons-lang-2.3.jar:$libdir/jwordpress/jwordpress-0.5.1-cli.jar:$libdir/jwordpress/jwordpress-0.5.1.jar:$libdir/jwordpress/jwordpress-0.5.jar:$libdir/jwordpress/xmlrpc-client-1.1.jar:$libdir/MathMLIO.jar:$libdir/paxtools-4.2.1-no-jena.jar:$libdir/sbml.jar:$libdir/transpath.jar:$libdir/VDAOEngine.jar:$libdir/xgmml.jar:$topdir
 
 java $JAVAOPTS -Dfile.encoding=UTF-8 -cp $classpath fr.curie.BiNoM.pathways.navicell.ProduceClickableMap $*
 
