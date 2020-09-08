131182fbff65464cfecb639bb3b84e959efad204
Vincent Noël <contact@vincent-noel.fr>
2020-09-08 15:11:32 +0200

    Improved selenium code for SBGNRenderer

--- src/main/java/fr/curie/navicell/SBGNRenderer.java
+++ src/main/java/fr/curie/navicell/SBGNRenderer.java
@@ -3,56 +3,67 @@ package fr.curie.navicell;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriverException;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.io.File;
-import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class SBGNRenderer {
     
     public static void render(String input, String output) {
         HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        // chromePrefs.put("webdriver.chrome.driver", "/usr/bin/chromedriver");
-        // chromePrefs.put("profile.default_content_settings.popups", 0);
         chromePrefs.put("download.default_directory", System.getProperty("user.dir"));
+        chromePrefs.put("download.prompt_for_download", false);
+        chromePrefs.put("download.directory_upgrade", true);
         
-        // System.setProperty("webdriver.chrome.logfile", "D:\\chromedriver.log");
-        // chromePrefs.put("webdriver.chrome.verboseLogging", "true");
-        
-        // chromePrefs.put("safebrowsing_for_trusted_sources_enabled", false);
-        // chromePrefs.put("safebrowsing.enabled", false);
-        // chromePrefs.put("whitelistedIps", "");
         System.setProperty("webdriver.chrome.whitelistedIps", "");
         
         ChromeOptions options = new ChromeOptions();
-        // options.setBinary("/usr/bin/chromium");
-        options.addArguments("--whitelisted-ips=''");
         options.addArguments("--headless");
-        options.addArguments("--window-size=1920x1080");
         options.addArguments("--disable-notifications");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--verbose");
-        // options.addArguments("--disable-web-security");
-        // options.addArguments("--allow-running-insecure-content");
-        // options.addArguments("--allow-insecure-localhost");
-
+        options.addArguments("--no-sandbox");        
         options.setExperimentalOption("prefs", chromePrefs);
-        WebDriver driver = new ChromeDriver(options);
-        System.out.println("http://navicell:8080/" + input);
-        driver.get("http://newt-converter?url=http://navicell:8080/" + input);
         
-        try {
-            TimeUnit.SECONDS.sleep(1);
-        }
-        catch (InterruptedException e) {
-            
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://newt-converter:8081?url=http://navicell/maps/" + input);
+        
+        WebDriverWait wait = new WebDriverWait(driver, 1000);
+        wait.withMessage("Timeout executing script: ");
+        
+        final Object[] out = new Object[1];
+        wait.until(
+            new ExpectedCondition<Boolean>() {
+                @Override
+                public Boolean apply(WebDriver driver) {
+                    
+                    try {
+                        out[0] = ((JavascriptExecutor) driver).executeScript("return document.sbgnReady");
+                    }
+                    catch (WebDriverException e) {
+                        // Still loading, not answering
+                        out[0] = null;
+                    }
+                    return (out[0] != null) && (out[0].toString() == "true");
+                }
+            }
+        );
+        
+        while( !Files.exists(Paths.get("truc.png")) ) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            }
+            catch (InterruptedException e) { }
         }
         File output_file = new File(output);
-        boolean res = new File("truc.png").renameTo(output_file);
-        System.out.println("Renamed ? " + res);
-        
+        new File("truc.png").renameTo(output_file);        
     }
     
 }
