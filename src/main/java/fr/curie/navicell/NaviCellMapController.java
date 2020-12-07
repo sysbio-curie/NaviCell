85cf3c9d5b246bc62fee0f7fada1272055967ea1
Vincent Noël <contact@vincent-noel.fr>
2020-12-07 13:13:45 +0100

    Added species type, search page

--- src/main/java/fr/curie/navicell/NaviCellMapController.java
+++ src/main/java/fr/curie/navicell/NaviCellMapController.java
@@ -1,4 +1,9 @@
 package fr.curie.navicell;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,11 +33,11 @@ import fr.curie.navicell.database.NaviCellSpecies;
 
 import fr.curie.navicell.storage.StorageProperties;
 import fr.curie.navicell.storage.StorageService;
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONArray;
 
-
-// @EnableConfigurationProperties(NaviCellProperties.class)
 @CrossOrigin(origins = "*", maxAge = 3600)
-// @EnableConfigurationProperties(StorageProperties.class)
 @RestController
 public class NaviCellMapController {
 
@@ -48,12 +53,19 @@ public class NaviCellMapController {
 	public NaviCellMapController(StorageService storageService) {
 		this.storageService = storageService;
 	}
+    
+  @GetMapping("/api/maps/{id}")
+  @ResponseStatus(value = HttpStatus.OK)
+  Optional<NaviCellMap> findMapById(@PathVariable("id") String id) {
+    return repository.findById(id);
+  }
   
   @GetMapping("/api/maps")
   @ResponseStatus(value = HttpStatus.OK)
   List<NaviCellMap> all() {
     return repository.findAll();
   }
+
 
   @DeleteMapping("/api/maps")
   @ResponseStatus(value = HttpStatus.OK)
@@ -69,6 +81,7 @@ public class NaviCellMapController {
     if (entry.isPresent()) {
       this.storageService.deleteByFolder(entry.get().folder);
       repository.deleteById(id);
+      species_repository.deleteByMapId(id);
     }
   }
   
@@ -78,6 +91,38 @@ public class NaviCellMapController {
     try{
       NaviCellMap entry = new NaviCellMap(this.storageService, name, network_file, species_repository);
       repository.save(entry);
+
+      String mapdata_path = this.storageService.getLocation() + "/" + entry.folder + "/_common/master_mapdata.json";
+      System.out.println("Mapdata path : " + mapdata_path);
+      
+      try{
+        JSONArray arr = new JSONArray(Files.readString(Paths.get(mapdata_path)));
+        for (int i = 0; i < arr.length(); i++)
+        {        
+          String t_class = arr.getJSONObject(i).getString("class");
+          if (
+            t_class.equals("PROTEIN") ||
+            t_class.equals("GENE") ||
+            t_class.equals("RNA") ||
+            t_class.equals("ANTISENSE_RNA") ||
+            t_class.equals("PHENOTYPE")
+          ) {
+            JSONArray species = arr.getJSONObject(i).getJSONArray("entities");
+            for (int j=0; j < species.length(); j++) {
+              String sname = species.getJSONObject(j).getString("name");
+              NaviCellSpecies t_sp = new NaviCellSpecies(sname, t_class, entry.id);
+              species_repository.save(t_sp);         
+            }
+          }
+        }
+      }
+      catch (IOException e) {
+        System.out.println(e.getMessage());
+      }
+      catch (JSONException e){
+        System.out.println(e.getMessage());
+      }
+      
     }
     catch (NaviCellMapException e) {
       throw new NaviCellMapControllerException(e.getMessage());
@@ -89,4 +134,23 @@ public class NaviCellMapController {
   List<NaviCellSpecies> all_species() {
     return species_repository.findAll();
   }
+  
+  @GetMapping("api/species/name/{name}")
+  @ResponseStatus(value = HttpStatus.OK)
+  List<NaviCellSpecies> speciesByName(@PathVariable("name") String name) {
+    return species_repository.findByName(name);
+  }
+  
+  @GetMapping("api/species/type/{type}")
+  @ResponseStatus(value = HttpStatus.OK)
+  List<NaviCellSpecies> speciesByType(@PathVariable("type") String type) {
+    return species_repository.findByType(type);
+  }
+  
+  @GetMapping("api/species/type/{type}/name/{name}")
+  @ResponseStatus(value = HttpStatus.OK)
+  List<NaviCellSpecies> speciesByName(@PathVariable("type") String type, @PathVariable("name") String name) {
+    return species_repository.findByTypeAndName(type, name);
+  }
+  
 }
--- src/main/java/fr/curie/navicell/database/NaviCellSpeciesRepository.java
+++ src/main/java/fr/curie/navicell/database/NaviCellSpeciesRepository.java
@@ -7,4 +7,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface NaviCellSpeciesRepository extends MongoRepository<NaviCellSpecies, String> {
 
   public List<NaviCellSpecies> findByName(String name);
+  public List<NaviCellSpecies> findByType(String type);
+  public List<NaviCellSpecies> findByMapId(String mapId);
+  public List<NaviCellSpecies> findByTypeAndName(String type, String name);
+  
+  public void deleteByMapId(String mapId);
 }
